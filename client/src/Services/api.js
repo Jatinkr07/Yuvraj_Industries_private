935ad811dda4e44d4a597092cef5da3c4a731577
Jatinkr07 <jatinkumar07911@gmail.com>
2025-02-24 18:47:36 +0530

    admin and dealer login CORS fix

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_URL = " https://yuvraj-industries-private.onrender.com";
-export const API_URL = " http://localhost:5500";
+export const API_URL = "https://yuvraj-industries-private-1.onrender.com";
+// export const API_URL = " http://localhost:5500";
 
 export const createCategory = (formData) =>
   axios.post(`${API_URL}/api/categories`, formData);
