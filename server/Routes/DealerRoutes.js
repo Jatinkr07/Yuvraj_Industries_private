bafa587e8b86bd0c6d797cde6145088ffd9676b3
Jatinkr07 <jatinkumar07911@gmail.com>
2025-03-22 11:16:16 +0530

    changes in dealer and subdealer

--- server/Routes/DealerRoutes.js
+++ server/Routes/DealerRoutes.js
@@ -36,8 +36,8 @@ router.post("/login", dealerLogin);
 router.get("/list", getDealers);
 router.delete("/:id", isAuthenticated, deleteDealer);
 router.put("/:id", isAuthenticated, updateDealer);
-router.get("/products", isDealerAuthenticated, getDealerProducts);
-router.get("/products/:dealerId", isAuthenticated, getDealerProducts);
+router.get("/products", getDealerProducts);
+router.get("/products/:dealerId", getDealerProducts);
 router.post("/password/request", requestPasswordChange);
 router.put("/password/:id", updateDealerPasswordByAdmin);
 router.get("/v1/list/dealer", isAuthenticated, getDealers);
@@ -48,14 +48,14 @@ router.post("/subdealer/create", isDealerAuthenticated, createSubDealer);
 router.get("/subdealer/:subDealerId/products", getSubDealerProducts);
 router.post("/subdealer/login", subDealerLogin);
 
-router.get("/subdealer/subdealers", isDealerAuthenticated, getSubDealers);
+router.get("/subdealer/subdealers", getSubDealers);
 router.put("/subdealer/subdealer/:id", isDealerAuthenticated, updateSubDealer);
 router.delete(
   "/subdealer/subdealer/:id",
   isDealerAuthenticated,
   deleteSubDealer
 );
-router.get("/subdealer/products", authenticateSubDealer, getSubDealerProducts);
+router.get("/subdealer/products", getSubDealerProducts);
 router.get("/v1/subdealer/all/list", getAllSubDealers);
 router.post("/subdealer/password/request", requestSubDealerPasswordChange);
 router.put(
